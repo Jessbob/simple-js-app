9465a2a71a133a963195f9b8d4545771ae91500c
JessBob <54863103+Jessbob@users.noreply.github.com>
2019-09-30 15:50:28 -0500

    created repository and added 4 pokemon with their name, height and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello world");
-
-var favoriteFood = "Filet Mignon";
-document.write(favoriteFood);
+var repository = [
+  { name: "Dugtrio", height: 2, types: ["ground"] },
+  { name: "Meowth", height: 1, types: ["normal"] },
+  { name: "Mankey", height: 1, types: ["fighting"] },
+  { name: "Charizard", height: 5, types: ["fire", "flying"] }
+];
