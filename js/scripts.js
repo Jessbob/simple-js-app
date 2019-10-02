76bf49717bdcdffd3e4dfa18b809dbeaa78dd840
JessBob <54863103+Jessbob@users.noreply.github.com>
2019-10-01 20:26:00 -0500

    added code to display pokemon names height and type. Trying to figure out how to highlight the largest one.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,26 @@ var repository = [
   { name: "Mankey", height: 1, types: ["fighting"] },
   { name: "Charizard", height: 5, types: ["fire", "flying"] }
 ];
+
+for (var i = 0; i < repository.length; i++) {
+  var size;
+  if (repository[i].height > 4);
+  {
+    size = "Wow that's a big one!";
+  }
+
+  document.write(
+    "Name: " +
+      repository[i].name +
+      " (Height: " +
+      repository[i].height +
+      ")" +
+      " - " +
+      size +
+      "<br>" +
+      " Type: " +
+      repository[i].types +
+      "<br>" +
+      "<br>"
+  );
+}
