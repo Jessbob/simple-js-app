b36250015d03767f87d152b31c257bd370929d7d
JessBob <54863103+Jessbob@users.noreply.github.com>
2019-10-03 12:49:29 -0500

    wrapped repository in an IIFE and created functions to get all and add pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-var repository = [
-  { name: "Dugtrio", height: 2, types: ["ground"] },
-  { name: "Meowth", height: 1, types: ["normal"] },
-  { name: "Mankey", height: 1, types: ["fighting"] },
-  { name: "Charizard", height: 5, types: ["fire", "flying"] }
-];
+var pokemonRepository = (function() {
+  var repository = [
+    { name: "Dugtrio", height: 2, types: ["ground"] },
+    { name: "Meowth", height: 1, types: ["normal"] },
+    { name: "Mankey", height: 1, types: ["fighting"] },
+    { name: "Charizard", height: 5, types: ["fire", "flying"] }
+  ];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 // for (var i = 0; i < repository.length; i++) {
-repository.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   var size;
   if (pokemon.height > 4) {
     size = " - Wow that's a big one!";
